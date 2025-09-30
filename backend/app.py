dcee0a4ada67138990b6c2cd726e9ce82b382b48
pendkarsaimanish <pendkarsaimanish@outlook.com>
2025-09-30 22:22:27 +0530

    .

--- backend/app.py
+++ backend/app.py
@@ -1,10 +1,17 @@
 from faster_whisper import WhisperModel
+from google import genai
+
+client = genai.Client(api_key="")
 
 model_size="tiny"
 
-model = WhisperModel(model_size, device="cpu")
+model = WhisperModel(model_size, device="cuda")
 
 segments, _ = model.transcribe(r"D:\react\ai_meeting_summarizer\backend\council_meeting.mp3")
 segments = list(segments)
 
-print(segments)
+res = client.models.generate_content()
+
+for segment in segments:
+    print(segment.text)
+
